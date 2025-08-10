d15695a7c64fa8b08ac251a614251337e93c509d
Kevin Ong <kevinongg@gmail.comm>
2025-08-09 23:17:55 -0400

    finished code, add css later

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,64 @@ export default function App() {
   console.log(episodes);
 
   /** DETAILED INFORMATION ABOUT THE SELECTED EPISODE */
-  /* Create a function using global params that displays the episode details when clicked
-    /* Create an if statement that if the selected episode is not true, then return
-    /* return....
-      /* a "section" and set the classname
-        /* an "h2" with "Episode Details"
-        /* a "p" tag with "Select an episode to learn more"
+  // Create a function using global params that displays the episode details when clicked
+  const EpisodeDetails = () => {
+    // Create an if statement that if the selected episode is not true, then return
+    if (!selectedEpisode) {
+      // return....
+      return (
+        // a "section" and set the classname
+        // an "h2" with "Episode Details"
+        // a "p" tag with "Select an episode to learn more"
+        <section className="episode-details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more</p>
+        </section>
+      );
+    }
     ///// *** RETURN THE PUPPY DETAILS NOW *** /////
-    /* return...
-      /* a "section" and set the classname
-        /* an "h2" with the selectedEpisode.id
-        /* a "h3" with  selectedEpisode.title
-        /* a "p" with selectedEpisode.description
-        /* a "button" with the text "Watch now"
-  /* END
-
-
+    // return...
+    return (
+      // a "section" and set the classname
+      // an "h2" with the selectedEpisode.id
+      // a "h3" with  selectedEpisode.title
+      // a "p" with selectedEpisode.description
+      // a "button" with the text "Watch now"
+      <section className="episode-details">
+        <h2>Episode {selectedEpisode.id}</h2>
+        <h3>{selectedEpisode.title}</h3>
+        <p>{selectedEpisode.description}</p>
+        <button>Watch now</button>
+      </section>
+      // END
+    );
+  };
 
   // TIPS: onClick{() => }
   /** LIST OF EPISODE THAT THE USER CAN SELECT */
   /* Create a function using global params that displays the episodes names and add an event listener
-     to display episode details when it is click.
-    /* Return...
+    to display episode details when it is click. */
+  const EpisodeNameList = () => {
+    // Return...
+    return (
       /* create a "section" and set the classname.
-        /* create the "h2"
-        /* create the "ul" and set the classname
-          /* map episodes, use brackets to map
-          /* create an LI to set the episode name in each LI and make an event listener 
-             "onclick" to change the selected episode state (setEpisodeList(episode). also set
-             a key with the episode id(episode.id)*/
+            /* create the "h2" and name it "Episodes"
+            /* create the "ul" and set the classname
+              /* map the state episodes, use brackets to map
+              /* create an LI to set the episode title in each LI and make an event listener in the LI
+                 "onclick" to update the selectedEpisode state (setSelectedEpisode(episode). also set
+                 a key with the episode id(episode.id)*/
+      <section>
+        <h2>Episodes</h2>
+        <ul className="episode-list"></ul>
+        {episodes.map((episode) => (
+          <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+            {episode.title}
+          </li>
+        ))}
+      </section>
+    );
+  };
   /* END
 
   
@@ -46,7 +75,10 @@ export default function App() {
       <header>
         <h1>Dark Echoes</h1>
       </header>
-      <main></main>
+      <main>
+        <EpisodeNameList />
+        <EpisodeDetails />
+      </main>
     </>
   );
 }
